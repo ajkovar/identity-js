852540e4bc1ac5d58453f3d7d0f7e066de15b2c3
Alex Kovar <ajkovar@gmail.com>
2011-01-09 09:38:01 -0600

    reformat

--- public/javascripts/is.js
+++ public/javascripts/is.js
@@ -1,85 +1,85 @@
 (function() {
 
-  var is = function(param) {
-    return new Wrapper(param)
-  }
-
-  var Wrapper = function(o) {
-    this.o = o;
-  }
-
-  Wrapper.prototype = {
-    equalTo: function(other) {
-      var obj=this.o;
-
-      if(obj===other) {
-        return true
-      }
-
-      // handle undefined vs null craziness
-      if(obj === undefined || other === undefined
-         || obj == null || other == null) {
-        return false
-      }
-
-      if(obj.constructor === Array) {
-        return arrayEquals(obj, other)
-      } 
-      if(obj.constructor === Object) {
-        return objectEquals(obj, other)
-      } 
-      return false
-    },
-      empty: function() {
-          var obj = this.o;
-          if(!obj) {
-              return true;
-          }
-          if(obj.constructor===Object) {
-              var count = 0;
-              for(var i in obj) {
-                  count++
-              }
-              if(count<=0) {
-                  return true
-              }
-          }
-          else if(obj.constructor===Array) {
-              if(obj.length<=0) {
-                  return true
-              }  
-          }
-          return false
-      }
-  }
-
-  var arrayEquals = function(a1, a2) {
-    if(a1.length!==a2.length) {
-      return false
+    var is = function(param) {
+        return new Wrapper(param)
     }
 
-    for(var i=0, length=a2.length;i<length;i++) {
-        if(!is(a1[i]).equalTo(a2[i])) {
-          return false
+    var Wrapper = function(o) {
+        this.o = o;
+    }
+
+    Wrapper.prototype = {
+        equalTo: function(other) {
+            var obj=this.o;
+
+            if(obj===other) {
+                return true
+            }
+
+            // handle undefined vs null craziness
+            if(obj === undefined || other === undefined
+               || obj == null || other == null) {
+                return false
+            }
+
+            if(obj.constructor === Array) {
+                return arrayEquals(obj, other)
+            } 
+            if(obj.constructor === Object) {
+                return objectEquals(obj, other)
+            } 
+            return false
+        },
+        empty: function() {
+            var obj = this.o;
+            if(!obj) {
+                return true;
+            }
+            if(obj.constructor===Object) {
+                var count = 0;
+                for(var i in obj) {
+                    count++
+                }
+                if(count<=0) {
+                    return true
+                }
+            }
+            else if(obj.constructor===Array) {
+                if(obj.length<=0) {
+                    return true
+                }  
+            }
+            return false
         }
     }
-    return true
-  }
 
-  var objectEquals = function(o1, o2) {
-    for(var i in o1) {
-      if(!is(o1[i]).equalTo(o2[i])) {
-        return false
-      }
-    }
-    for(var i in o2) {
-      if(!is(o2[i]).equalTo(o1[i])) {
-        return false
-      }
-    }
-    return true
-  }
+    var arrayEquals = function(a1, a2) {
+        if(a1.length!==a2.length) {
+            return false
+        }
 
-  window.is=is
+        for(var i=0, length=a2.length;i<length;i++) {
+            if(!is(a1[i]).equalTo(a2[i])) {
+                return false
+            }
+        }
+        return true
+    }
+
+    var objectEquals = function(o1, o2) {
+        for(var i in o1) {
+            if(!is(o1[i]).equalTo(o2[i])) {
+                return false
+            }
+        }
+        for(var i in o2) {
+            if(!is(o2[i]).equalTo(o1[i])) {
+                return false
+            }
+        }
+        return true
+    }
+
+    window.is=is
 
 } ())
--- spec/javascripts/IsSpec.js
+++ spec/javascripts/IsSpec.js
@@ -1,81 +1,81 @@
 describe("is", function() {
-  describe("equalTo", function() {
-    it("should handle native values", function() {
-      expect(is(1).equalTo(1)).toBe(true)
-      expect(is("abc").equalTo("abc")).toBe(true)
+    describe("equalTo", function() {
+        it("should handle native values", function() {
+            expect(is(1).equalTo(1)).toBe(true)
+            expect(is("abc").equalTo("abc")).toBe(true)
+        })
+
+        it("should compare functions by reference", function() {
+            var f = function() {}
+            expect(is(f).equalTo(f)).toBe(true)
+            expect(is(f).equalTo(function() {})).toBe(false)
+        })
+
+        describe("handling non values", function() {
+            it("should treat nulls as equal", function() {
+                expect(is(null).equalTo(null)).toBe(true)
+            })
+
+            it("should treat undefineds as equal", function() {
+                expect(is(undefined).equalTo(undefined)).toBe(true)
+            })
+
+            it("should recognise difference between null and undefined", function() {
+                var equals;
+                equals =  is(null).equalTo(undefined)
+                expect(equals).toBe(false)
+                equals =  is(undefined).equalTo(null)
+                expect(equals).toBe(false)
+            })
+        })
+
+        describe("comparing objects", function() {
+            it("should return true for objects of equal value", function() {
+                var equals = is({a:"a"}).equalTo({a:"a"})
+                expect(equals).toBe(true)
+            })
+
+            it("should return false for objects not of equal value", function() {
+                var equals = is({a:"a"}).equalTo({a:"asdf"})
+                expect(equals).toBe(false)
+                equals = is({b:"a"}).equalTo({a:"a"})
+                expect(equals).toBe(false)
+            })
+
+            it("should ignore property order", function() {
+                var equals = is({a:"a", b:"b"}).equalTo({b:"b", a:"a"})
+                expect(equals).toBe(true)
+            })
+        })
+
+        describe("comparing arrays", function() {
+
+            it("should return true for equal arrays", function() {
+                var equals = is([1,2]).equalTo([1,2])
+                expect(equals).toBe(true)
+            });
+
+            it("should return false for non equal arrays", function() {
+                var equals = is([1,2]).equalTo([3,4])
+                expect(equals).toBe(false)
+            });
+
+            it("should return false for partial equality", function() {
+                var equals = is([1,2]).equalTo([1])
+                expect(equals).toBe(false)
+
+                equals = is([1]).equalTo([1,2])
+                expect(equals).toBe(false)
+            });
+
+            it("should handle nested arrays", function() {
+                var equals = is([1, [2, 3]]).equalTo([1, [2, 3]])
+                expect(equals).toBe(true)
+                equals = is([1, [2, 3]]).equalTo([1, [2]])
+                expect(equals).toBe(false)
+            })
+        })
     })
-
-    it("should compare functions by reference", function() {
-      var f = function() {}
-      expect(is(f).equalTo(f)).toBe(true)
-      expect(is(f).equalTo(function() {})).toBe(false)
-    })
-
-    describe("handling non values", function() {
-      it("should treat nulls as equal", function() {
-        expect(is(null).equalTo(null)).toBe(true)
-      })
-
-      it("should treat undefineds as equal", function() {
-        expect(is(undefined).equalTo(undefined)).toBe(true)
-      })
-
-      it("should recognise difference between null and undefined", function() {
-        var equals;
-        equals =  is(null).equalTo(undefined)
-        expect(equals).toBe(false)
-        equals =  is(undefined).equalTo(null)
-        expect(equals).toBe(false)
-      })
-    })
-
-    describe("comparing objects", function() {
-      it("should return true for objects of equal value", function() {
-        var equals = is({a:"a"}).equalTo({a:"a"})
-        expect(equals).toBe(true)
-      })
-
-      it("should return false for objects not of equal value", function() {
-        var equals = is({a:"a"}).equalTo({a:"asdf"})
-        expect(equals).toBe(false)
-        equals = is({b:"a"}).equalTo({a:"a"})
-        expect(equals).toBe(false)
-      })
-
-      it("should ignore property order", function() {
-        var equals = is({a:"a", b:"b"}).equalTo({b:"b", a:"a"})
-        expect(equals).toBe(true)
-      })
-    })
-
-    describe("comparing arrays", function() {
-
-      it("should return true for equal arrays", function() {
-        var equals = is([1,2]).equalTo([1,2])
-        expect(equals).toBe(true)
-      });
-
-      it("should return false for non equal arrays", function() {
-        var equals = is([1,2]).equalTo([3,4])
-        expect(equals).toBe(false)
-      });
-
-      it("should return false for partial equality", function() {
-        var equals = is([1,2]).equalTo([1])
-        expect(equals).toBe(false)
-
-        equals = is([1]).equalTo([1,2])
-        expect(equals).toBe(false)
-      });
-
-      it("should handle nested arrays", function() {
-        var equals = is([1, [2, 3]]).equalTo([1, [2, 3]])
-        expect(equals).toBe(true)
-        equals = is([1, [2, 3]]).equalTo([1, [2]])
-        expect(equals).toBe(false)
-      })
-    })
-  })
 
     describe("empty", function() {
         it("should recognize empty strings", function() {
